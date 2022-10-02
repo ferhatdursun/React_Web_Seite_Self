600c297d50cc617febfd36a0452a1d0480857ac3
Ferhat Dursun <ferhatdursnu@gmail.com>
2022-10-02 13:16:41 +0200

    React

--- src/Style/KontaktStyle.jsx
+++ src/Style/KontaktStyle.jsx
@@ -6,6 +6,13 @@ color: orange;
 font-size: larger;
 `
 
+export const Img_Fd = styled.img`
+width: 20rem;
+`
+
+export const Gmail_Icon = styled.img`
+width: 3rem;
+`
 
 export const Container = styled.div`
   /* background: url(https://picsum.photos/1600/900); */
@@ -22,10 +29,21 @@ export const Div = styled.div`
   align-items: center;
   text-align: center;
   justify-content: center;
-  padding: 13% 0;
+  padding: 3% 0;
   /* margin: 17% 0; */
   
 `;
 export const E = styled.i`
   color: blueviolet;
+`;
+
+export const Container_Kontakt = styled.div`
+display: flex;
+margin: 1rem;
+`
+export const Div_Kontakt_Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 1rem;
 `;
--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -1,35 +1,55 @@
 import React from "react";
-import { Container, E, Div, A } from "../Style/KontaktStyle";
-
+import {
+  Container,
+  E,
+  Div,
+  A,
+  Gmail_Icon,
+  Img_Fd,
+  Container_Kontakt,
+  Div_Kontakt_Details
+} from "../Style/KontaktStyle";
+import Gmail from "../assets/Gmail.png";
+import Ferhat_Foto from "../assets/fd.png";
 const Kontakt = () => {
   return (
     <Container>
       <Div>
-        <h3>
+        <Img_Fd src={Ferhat_Foto} alt="" />
+        {/* <h3>
           F<E>e</E>rhat Dursun
-        </h3>
-        <A href="mailto:Ferhatdursnu@gmail.com">Send email</A>
-        <i class="bi bi-envelope"></i>
-        <h4>
+        </h3> */}
+        <Container_Kontakt>
+          <Div_Kontakt_Details>
+            <A href="mailto:Ferhatdursnu@gmail.com">Send email</A>
+            <Gmail_Icon src={Gmail} alt="" />
+          </Div_Kontakt_Details>
+          {/* <i class="bi bi-envelope"></i> */}
+          {/* <h4>
           F<E>e</E>rhatdursnu@gmail.com
-        </h4>
-        <br />
-        <A
-          href="https://github.com/ferhatdursun"
-          target="_blank"
-          rel="noreferrer"
-          //! Burada ki rel="noreferrer" target'in calismasi icin.
-        >
-          Github{" "}
-        </A>
-        <br />
-        <A
-          href="https://www.linkedin.com/in/ferhat-dursun1/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Linkedin
-        </A>
+        </h4> */}
+          <br />
+          <Div_Kontakt_Details>
+            <A
+              href="https://github.com/ferhatdursun"
+              target="_blank"
+              rel="noreferrer"
+              //! Burada ki rel="noreferrer" target'in calismasi icin.
+            >
+              Github{" "}
+            </A>
+          </Div_Kontakt_Details>
+          <br />
+          <Div_Kontakt_Details>
+            <A
+              href="https://www.linkedin.com/in/ferhat-dursun1/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Linkedin
+            </A>
+          </Div_Kontakt_Details>
+        </Container_Kontakt>
       </Div>
     </Container>
   );
